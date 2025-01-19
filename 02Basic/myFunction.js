44c0fea5b35ad16ebc50d6b14ef513e0e5cf5ad6
--global <abhishekchauhan3003@gmail.com>
2025-01-19 11:32:09 +0530

    fn in ts

--- 02Basic/myFunction.js
+++ 02Basic/myFunction.js
@@ -1,6 +1,4 @@
-function addTwo(num) {
-    return num + 2;
-}
+return num + 2;
 addTwo(2);
 // optional passing 
 function checkUser(name, age, isUser) {
@@ -8,3 +6,14 @@ function checkUser(name, age, isUser) {
     return name + 'age of ' + age;
 }
 checkUser('shubha', 2);
+var hero = ['hero1', 'hero2'];
+hero.map(function (hero) {
+    return hero;
+});
+var returnVoid = function (value) {
+    console.log('>', value);
+};
+var handleException = function (value) {
+    console.log('>', value);
+    throw value;
+};
--- 02Basic/myFunction.ts
+++ 02Basic/myFunction.ts
@@ -11,3 +11,23 @@ function checkUser(name:string,age:number,isUser:boolean=false){
 }
 
 checkUser('shubha',2)
+
+
+
+const hero =['hero1','hero2']
+
+hero.map((hero : string) :string=>{
+    return hero
+})
+
+
+const returnVoid=(value:string) : void=>{
+    console.log('>',value)
+
+}
+
+
+const handleException=(value:any):never=>{
+    console.log('>',value)
+    throw value
+}
